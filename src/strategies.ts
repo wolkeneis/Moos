e60cd5083be0a29a15e880a2617f3e06b3288742
Simon <simon@benezan.de>
2022-05-03 21:02:21 +0200

    simplify strategy

--- src/strategies.ts
+++ src/strategies.ts
@@ -72,103 +72,34 @@ passport.use(
       passReqToCallback: true
     },
     async (req: Request, accessToken: string, refreshToken: string, profile: Profile, done) => {
-      if (req.cookies.session) {
-        verifyCookie(req.cookies.session).then((token) => {
-          if (token) {
-            database
-              .userProviderProfileUpdateOrCreate({
-                provider: "discord",
-                uid: token.uid,
-                providerId: profile.id,
-                username: profile.username + "#" + profile.discriminator,
-                avatar: profile.avatar ? `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png` : null,
-                accessToken: accessToken,
-                refreshToken: refreshToken
-              })
-              .then((user) => done(null, { uid: user.uid }))
-              .catch(done);
+      try {
+        const token = await verifyCookie(req.cookies.session);
+        let uid: string;
+        if (token) {
+          uid = token.uid;
+        } else {
+          const foundProfile = await database.userProviderProfileFindById({ provider: profile.provider, providerId: profile.id });
+          if (foundProfile) {
+            uid = foundProfile.uid;
           } else {
-            database
-              .userProviderProfileFindById({ provider: profile.provider, providerId: profile.id })
-              .then((foundProfile) => {
-                if (foundProfile) {
-                  database
-                    .userProviderProfileUpdateOrCreate({
-                      provider: "discord",
-                      uid: foundProfile.uid,
-                      providerId: profile.id,
-                      username: profile.username + "#" + profile.discriminator,
-                      avatar: profile.avatar ? `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png` : null,
-                      accessToken: accessToken,
-                      refreshToken: refreshToken
-                    })
-                    .then((user) => done(null, { uid: user.uid }))
-                    .catch(done);
-                } else {
-                  createUser(
-                    profile.username + "#" + profile.discriminator,
-                    profile.avatar ? `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png` : null
-                  )
-                    .then((uid) => {
-                      database
-                        .userProviderProfileUpdateOrCreate({
-                          provider: "discord",
-                          uid: uid,
-                          providerId: profile.id,
-                          username: profile.username + "#" + profile.discriminator,
-                          avatar: profile.avatar ? `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png` : null,
-                          accessToken: accessToken,
-                          refreshToken: refreshToken
-                        })
-                        .then((user) => done(null, { uid: user.uid }))
-                        .catch(done);
-                    })
-                    .catch(done);
-                }
-              })
-              .catch(done);
+            uid = await createUser(
+              profile.username + "#" + profile.discriminator,
+              profile.avatar ? `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png` : null
+            );
           }
+        }
+        const user = await database.userProviderProfileUpdateOrCreate({
+          provider: "discord",
+          uid: uid,
+          providerId: profile.id,
+          username: profile.username + "#" + profile.discriminator,
+          avatar: profile.avatar ? `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png` : null,
+          accessToken: accessToken,
+          refreshToken: refreshToken
         });
-      } else {
-        database
-          .userProviderProfileFindById({ provider: profile.provider, providerId: profile.id })
-          .then((foundProfile) => {
-            if (foundProfile) {
-              database
-                .userProviderProfileUpdateOrCreate({
-                  provider: "discord",
-                  uid: foundProfile.uid,
-                  providerId: profile.id,
-                  username: profile.username + "#" + profile.discriminator,
-                  avatar: profile.avatar ? `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png` : null,
-                  accessToken: accessToken,
-                  refreshToken: refreshToken
-                })
-                .then((user) => done(null, { uid: user.uid }))
-                .catch(done);
-            } else {
-              createUser(
-                profile.username + "#" + profile.discriminator,
-                profile.avatar ? `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png` : null
-              )
-                .then((uid) => {
-                  database
-                    .userProviderProfileUpdateOrCreate({
-                      provider: "discord",
-                      uid: uid,
-                      providerId: profile.id,
-                      username: profile.username + "#" + profile.discriminator,
-                      avatar: profile.avatar ? `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png` : null,
-                      accessToken: accessToken,
-                      refreshToken: refreshToken
-                    })
-                    .then((user) => done(null, { uid: user.uid }))
-                    .catch(done);
-                })
-                .catch(done);
-            }
-          })
-          .catch(done);
+        done(null, { uid: user.uid });
+      } catch (error) {
+        done(error as Error);
       }
     }
   )
