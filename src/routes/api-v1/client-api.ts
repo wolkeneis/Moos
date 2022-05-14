d85a2f694465020b2f232ec6391834db757e1d16
Simon <simon@benezan.de>
2022-05-14 18:00:34 +0200

    Update client-api.ts

--- src/routes/api-v1/client-api.ts
+++ src/routes/api-v1/client-api.ts
@@ -1,8 +1,24 @@
+import { User } from "database/database-adapter";
 import express, { Router } from "express";
+import { v1 } from "moos-api";
 import passport from "passport";
 
 const router: Router = express.Router();
 
 router.use(passport.authenticate("bearer", { session: false }));
+
+router.post("/profile", async (req, res) => {
+  const profile = req.user as User;
+  const response: v1.components["schemas"]["UserProfile"] = {
+    uid: profile.uid,
+    username: profile.username,
+    avatar: profile.avatar,
+    scopes: profile.scopes,
+    private: profile.private,
+    clients: profile.clients,
+    creationDate: profile.creationDate
+  };
+  res.json(response);
+});
 
 export default router;
